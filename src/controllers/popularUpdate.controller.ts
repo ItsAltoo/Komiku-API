a3f48555cfc6ccfc4ff8e80a51e21dfc2c56e7b3
itsMalik <itsmalik57@gmail.com>
2026-05-23 22:45:41 +0800

    [KA-4] Add popular update endpoint controller and service

---
+++ src/controllers/popularUpdate.controller.ts
@@ -0,0 +1,27 @@
+import type { Request, Response } from "express";
+import { errorResponse, successResponse } from "../lib/utils/response.js";
+import { popularUpdateService } from "../services/getPopularUpdate.service.js";
+
+export const getPopularUpdate = async (req: Request, res: Response) => {
+  try {
+    const { type } = req.query;
+
+    const validTypes = ["manga", "manhwa", "manhua"];
+    const selectedType =
+      typeof type === "string" && validTypes.includes(type) ? type : "all";
+
+    const { data } = await popularUpdateService(selectedType);
+
+    return successResponse({
+      res,
+      message: `Successfully fetched popular update manga of type ${selectedType}`,
+      data: data,
+    });
+  } catch (error) {
+    return errorResponse({
+      res,
+      message: "Failed to fetch ranking manga",
+      error: error instanceof Error ? error.message : String(error),
+    });
+  }
+};
